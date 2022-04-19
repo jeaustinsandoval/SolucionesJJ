b8f552137ea9ca04939c99714bd3d75dee2883b8
JEAUSTIN JONAIKER SANDOVAL <jsandovals0748@uvirtual.ac.cr>
2022-04-19 15:44:02 -0600

    Se modica ENTITY

---
+++ Entity/dbo/ClienteEntity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+   
+    public class ClienteEntity : DBEntity
+    {
+        public int? IdCliente { get; set; }
+        public string Identificacion { get; set; }
+        public int? IdTipoIdentificacion { get; set; }
+        public string Nombre { get; set; }
+        public string PrimerApellido { get; set; }
+        public string SegundoApellido { get; set; }
+        public DateTime FechaNacimiento { get; set; } = DateTime.Now;
+        public int? Nacionalidad { get; set; }
+        public DateTime FechaDefuncion { get; set; } = DateTime.Now;
+        public string Genero { get; set; }
+        public string NombreApellidosPadre { get; set; }
+        public string NombreApellidosMadre { get; set; }
+        public string Pasaporte { get; set; }
+        public string CuentaIBAN { get; set; }
+        public string CorreoNotifica { get; set; }
+
+    }
+}
---
+++ Entity/dbo/SolicitudEntity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+
+    public class SolicitudEntity : DBEntity
+
+    {
+        public SolicitudEntity()
+        {
+            Cliente = Cliente ?? new ClienteEntity();
+            Servicio = Servicio ?? new ServicioEntity();
+        }
+        
+        public int? IdSolicitud{ get; set; }
+        public int? IdCliente { get; set; }
+        public virtual ClienteEntity Cliente { get; set; }
+        public int? IdServicio { get; set; }
+        public virtual ServicioEntity Servicio { get; set; }
+        public int? Cantidad { get; set; }
+        public int? Monto { get; set; }
+        public DateTime FechaEntrega { get; set; } = DateTime.Now;
+        public string UsuarioEntrega { get; set; }
+        public string Observaciones { get; set; }
+    }
+}
