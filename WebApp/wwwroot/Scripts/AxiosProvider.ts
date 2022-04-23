ff97e4fb8ab4c8931ba4d559fc4cd33e6155bbe5
jvargasGTI <jvargas@gticr.com>
2022-04-22 18:35:37 -0600

    se actualiza la variable id del Axios

--- WebApp/wwwroot/Scripts/AxiosProvider.ts
+++ WebApp/wwwroot/Scripts/AxiosProvider.ts
@@ -1,5 +1,5 @@
 ﻿namespace App.AxiosProvider   {
-    export const EliminarCliente = (idc) => axios.delete<DBEntity>("Cliente/Grid?handler=Eliminar&id=" + id).then(({ data }) => data);
+    export const EliminarCliente = (id) => axios.delete<DBEntity>("Cliente/Grid?handler=Eliminar&id=" + id).then(({ data }) => data);
     export const GuardarCliente = (entity) => axios.post<DBEntity>("Cliente/Edit", entity).then(({ data }) => data);
     export const EliminarSolicitud = (id) => axios.delete<DBEntity>("Solicitud/Grid?handler=Eliminar&id=" + id).then(({ data }) => data);
     export const GuardarSolicitud = (entity) => axios.post<DBEntity>("Solicitud/Edit", entity).then(({ data }) => data);
